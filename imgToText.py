12a85ddbb75d2bdcd8792ba3193b91386b3ec224
vinay-mane <manevinay44+github@gmail.com>
2022-11-05 15:11:05 +0530

    v0.17

--- imgToText.py
+++ imgToText.py
@@ -1,7 +1,8 @@
-# import pytesseract
+import pytesseract
+import cv2
 
-# def getText(image):
-#   return(pytesseract.image_to_string(image))
+def getText(image):
+  return(pytesseract.image_to_string(image))
 
-# image = cv2.imread("j.png")
-# getText(image)
+image = cv2.imread("j.png")
+getText(image)
